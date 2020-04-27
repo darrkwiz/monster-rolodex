cc6f5e3659aa6246136eefc47343079671acb655
Your darrkwiz <darrkwiz@gmail.com>
2020-04-27 20:10:39 +0400

    Updates

---
+++ precache-manifest.734bf114f25704b29421e895a29e5f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f501224429188adb71ec9f06f713f700",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5d8a71b2509bd7cad7df",
+    "url": "/monster-rolodex/static/css/main.a3b0d169.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monster-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8a71b2509bd7cad7df",
+    "url": "/monster-rolodex/static/js/main.39dad03b.chunk.js"
+  },
+  {
+    "revision": "1c79305e130eba318f28",
+    "url": "/monster-rolodex/static/js/runtime-main.5ec4cad5.js"
+  }
+]);
